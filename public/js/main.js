859f3e22a6078e5677b156f8f76d6235d8392699
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-17 20:05:36 -0600

    api connection setted

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+const hotelsData = [];
+// Work here!
+
+
+// Connect to Api
+const fetchApiData = async (endpoint) => {
+  const api = 'https://63ed6ef13d9c852c3f5a8b83.mockapi.io/';
+  
+  try {
+    const response = await fetch(`${api}${endpoint}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      const apiData = await response.json();
+      hotelsData.push(apiData);
+
+      // TODO: send data to show in the HTML
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+
+// Here set endPoints
+const setEndPoint = async () => {
+  await fetchApiData('hotel');
+  await fetchApiData('rooms');
+};
+window.onload = () => setEndPoint();
