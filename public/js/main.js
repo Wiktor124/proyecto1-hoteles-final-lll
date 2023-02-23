ee875aa74a6c1d7a057936ad03e2728545c44bd7
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-23 09:22:15 -0600

    new api settings

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
+import {apiData} from './apiConnection.js'
 
+class Places {
+  constructor(places) {
+    this.places = places;
+  }
+
+  printPlaces() {
+    document.querySelector('.carrousel').innerHTML = this.places.map(item => {
+      return `
+        <li>
+          <a href="#" class="carrousel__links"></a>  
+          <img src="${item.heroImage}" alt="${item.placeName}">
+          <h3 class="carrousel__title">${item.placeName} <strong>TRAVEL</strong></h3>
+        </li>
+      `
+    }).join('');
+  }
+
+  printHotels() {
+    document.querySelector('.carr').innerHTML = this.places.map(item => {
+      return item
+    }).join('');
+  }
+
+  printRooms() {
+
+  }
+}
+export const print = new Places(apiData);
+print.printPlaces()
