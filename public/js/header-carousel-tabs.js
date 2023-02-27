b1f0931a7f58ee441251775daf056759e94e75dc
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-02-27 17:28:59 -0600

    links

--- public/js/header-carousel-tabs.js
+++ public/js/header-carousel-tabs.js
@@ -33,3 +33,15 @@ let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => 
     track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
   }
 }
+
+
+
+// header button
+const btn = document.getElementById('header-btn');
+btn.addEventListener('click', () => {
+  btn.classList.toggle('header__button--active')
+  document.querySelector('.header__links').classList.toggle('header__links--active')
+  console.log('hola');
+})
+
+console.log('hola');
--- public/js/header-carrousel.js
+++
@@ -1,9 +0,0 @@
-// header button
-const btn = document.getElementById('header-btn');
-btn.addEventListener('click', () => {
-  btn.classList.toggle('header__button--active')
-  document.querySelector('.header__links').classList.toggle('header__links--active')
-  console.log('hola');
-})
-
-console.log('hola');
