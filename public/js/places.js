ed4145dcaa92b593b730c93dd122a222b01d422f
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-03-01 23:29:18 -0600

    error links fixed

--- public/js/places.js
+++ public/js/places.js
@@ -5,9 +5,12 @@ function printPlaces(placesData) {
   document.getElementById('places').innerHTML  = placesData.map(item => {
    return `
     <li>
-      <h3 class="carrousel__title">${item.placeName}</h3>
-      <img src="${item.heroImage}" alt="${item.placeName}">
-      <p>${item.description}</p>
+      <a href="./place.html" id="carrousel-links" class="gallery__links" data-id="${item.id}"></a>
+      <div>
+        <h3 class="carrousel__title">${item.placeName}</h3>
+        <img src="${item.heroImage}" alt="${item.placeName}">
+        <p>${item.description}</p>
+      </div>
     </li>
     `
   }).join('')
