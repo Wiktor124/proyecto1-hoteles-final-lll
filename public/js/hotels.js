04f407d4837ec273bc446dd2e1391173e466e0e5
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-03-05 15:46:35 -0600

    bugs solved

--- public/js/hotels.js
+++ public/js/hotels.js
@@ -1,11 +1,13 @@
 import { hotels } from './apiConnection.js';
+import { placeOrHotel } from './events.js'
+placeOrHotel.clickEvents()
 
 // print hotels
 function printHotels(hotelsData) {
   document.getElementById('hotels').innerHTML  = hotelsData.map(item => {
    return `
     <li>
-      <a href="./hotel.html" id="carrousel-links" class="gallery__links" data-id="${item.id}"></a>
+      <a href="./hotel.html"class="gallery__link hotel__link" data-id="${item.id}"></a>
       <div>
         <img src="${item.image}" alt="${item.hotelName}">
         <h2>${item.hotelName}</h2>
