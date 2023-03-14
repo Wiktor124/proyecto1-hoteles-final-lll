9811d0b3f87da801a8108e515d699c5b7fce36f4
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-03-14 17:41:29 -0600

    create dom elements (gallery cards) (#18)

--- public/js/create-dom-elements.js
+++ public/js/create-dom-elements.js
@@ -1,4 +1,3 @@
-
 export class CreateDomElements {
   static createBanner() {
     const bannerDiv = document.createElement('div')
@@ -14,12 +13,46 @@ export class CreateDomElements {
   }
 
   static getImageAndText(route, text) {
-    CreateDomElements.createBanner();
-    
+    CreateDomElements.createBanner()
+
     const banner = document.querySelector('.banner')
     banner.style.background = `url(${route}) center no-repeat`
     banner.style.backgroundSize = 'cover'
 
     document.querySelector('.banner-title').textContent = text
   }
+
+  static createGalleryCards(routeImage, itemName, routePage) {
+
+    console.log(routeImage);
+    
+    const galleryLi = document.createElement('li')
+    galleryLi.style.background = `url(${routeImage})`
+
+    const divCard = document.createElement('div')
+
+    const spanText = document.createElement('span')
+    spanText.textContent = 'Costa Rica'
+    spanText.classList.add('gallery__country')
+
+    const title = document.createElement('h4')
+    title.classList.add('gallery__titleCard')
+    title.textContent = itemName
+
+    divCard.append(spanText, title)
+
+    const arrow = document.createElement('img')
+    arrow.classList.add('gallery__arrow')
+    arrow.src = ''
+    arrow.classList.add()
+
+    const link = document.createElement('a')
+    link.href = routePage
+    link.classList.add('gallery__link')
+    link.append(divCard, arrow)
+
+    galleryLi.appendChild(link)
+
+    return galleryLi.outerHTML
+  } 
 }
