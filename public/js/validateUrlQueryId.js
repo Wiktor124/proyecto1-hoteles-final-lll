ed5b547cc47c4387dc88b0bca6aebd5dcabf9060
Víctor Peña <victortkd88@gmail.com>
2023-04-25 08:55:45 -0600

    new properties

--- public/js/validateUrlQueryId.js
+++ public/js/validateUrlQueryId.js
@@ -1,15 +1,17 @@
 import { fetchPlaces, fetchHotels } from './apiConnection.js'
 
 // validate if the query param of the URL has an id
-export const validateUrlQueryId = async (id, typeData) => {
+const validateUrlQueryId = async (id, typeData) => {
 
   if(id < 0) throw new Error("There's not id!");
 
-  const data = {
+  const apiData = {
     places: await fetchPlaces(),
     hotels: await fetchHotels(),
     both: await Promise.all([fetchPlaces(), fetchHotels()])
   }
-  
-  return data[typeData]
+
+  return apiData[typeData]
 }
+
+export default validateUrlQueryId;
